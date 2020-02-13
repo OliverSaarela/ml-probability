3711b0e8c299c5c3399108dbe3c4084e6fbe9767
OliverSaarela <43965623+OliverSaarela@users.noreply.github.com>
2020-02-13 13:50:11 +0200

    Testidatan lisäys

--- data.py
+++ data.py
@@ -12,11 +12,16 @@ import os
 
 def main():
     train_data = Path('data/2000-2018-supershort.csv')
-    test_data = Path('')
+    test_data = Path('data/2019-2020-supershort.csv')
 
     
-    df = pd.read_csv(train_data)
-    print(df)
+    train_df = pd.read_csv(train_data)
+    test_df = pd.read_csv(test_data)
+    print(train_df.head())
+    print(test_df.head())
+
+    LABEL_COLUMN =  'Winner'
+    LABELS = [train_df['Player']]
 
 if __name__ == "__main__":
     main()
