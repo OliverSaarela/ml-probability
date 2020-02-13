f9aade999bff671582235f58a11d8be86193fa98
OliverSaarela <43965623+OliverSaarela@users.noreply.github.com>
2020-02-13 14:29:16 +0200

    Datan lukeminen Tensorflowlla

--- data.py
+++ data.py
@@ -6,22 +6,48 @@ import functools
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from pathlib import Path
-#import tensorflow_datasets as tfds
-import os
 
 def main():
-    train_data = Path('data/2000-2018-supershort.csv')
-    test_data = Path('data/2019-2020-supershort.csv')
+    train_data_path = 'data/2000-2018-supershort.csv'
+    test_data_path = 'data/2019-2020-supershort.csv'
 
     
-    train_df = pd.read_csv(train_data)
-    test_df = pd.read_csv(test_data)
+    train_df = pd.read_csv(train_data_path)
+    test_df = pd.read_csv(test_data_path)
     print(train_df.head())
     print(test_df.head())
 
     LABEL_COLUMN =  'Winner'
     LABELS = [train_df['Player']]
+
+    
+
+    def get_dataset(file_path, **kwargs):
+
+        dataset = tf.data.experimental.make_csv_dataset(
+            file_path,
+            batch_size = 5, #Small during testing, Make bigger when done.
+            label_name = LABEL_COLUMN,
+            na_value = '?',
+            num_epochs = 1,
+            ignore_errors = True,
+            **kwargs
+            )
+        return dataset
+
+    SELECT_COLUMNS = ['Player 1', 'Player 2', 'Winner', 'surface']
+
+    raw_train_data = get_dataset(train_data_path, select_columns=SELECT_COLUMNS)
+    raw_test_data = get_dataset(test_data_path, select_columns=SELECT_COLUMNS)
+
+    def show_batch(dataset):
+        for batch, label in dataset.take(1):
+            for key, value in batch.items():
+                print("{:20s}: {}".format(key,value.numpy()))
+
+    show_batch(raw_train_data)
+
+    
 
 if __name__ == "__main__":
     main()
