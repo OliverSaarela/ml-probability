8cacf578c8b0b5125f982182fe8816bbbbf5d799
OliverSaarela <43965623+OliverSaarela@users.noreply.github.com>
2020-02-26 14:26:21 +0200

    Malli rakennettu

--- data.py
+++ data.py
@@ -6,6 +6,7 @@ import functools
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
 
 def main():
     train_data_path = 'data/2000-2018-supershort.csv'
@@ -24,8 +25,8 @@ def main():
     train_df['Winner'].loc[train_df['Winner'] == train_df['Player_1']] = 1
     train_df['Winner'].loc[train_df['Winner'] == train_df['Player_2']] = 0
 
-    test_df['Winner'].loc[test_df['Winner'] == test_df['Player_1']] = 1
-    test_df['Winner'].loc[test_df['Winner'] == test_df['Player_2']] = 0
+    #test_df['Winner'].loc[test_df['Winner'] == test_df['Player_1']] = 1
+    #test_df['Winner'].loc[test_df['Winner'] == test_df['Player_2']] = 0
     
     
     # Change Player_1, Player_2 and surface to onehotencoding
@@ -33,21 +34,35 @@ def main():
     numeric_train_df['Player'] = pd.Categorical(numeric_train_df['Player'])
     numeric_train_df['Player'] = numeric_train_df.Player.cat.codes
 
-    numeric_test_df = pd.get_dummies(test_df, prefix=['Player_1', 'Player_2', 'Surface'], columns=['Player_1', 'Player_2', 'Surface'])
-    numeric_test_df['Player'] = pd.Categorical(numeric_test_df['Player'])
-    numeric_test_df['Player'] = numeric_test_df.Player.cat.codes
+    #numeric_test_df = pd.get_dummies(test_df, prefix=['Player_1', 'Player_2', 'Surface'], columns=['Player_1', 'Player_2', 'Surface'])
+    #numeric_test_df['Player'] = pd.Categorical(numeric_test_df['Player'])
+    #numeric_test_df['Player'] = numeric_test_df.Player.cat.codes
 
     numeric_train_df['Winner'] = pd.to_numeric(train_df['Winner'], downcast = 'integer')
-    numeric_test_df['Winner'] = pd.to_numeric(test_df['Winner'], downcast = 'integer')
+    #numeric_test_df['Winner'] = pd.to_numeric(test_df['Winner'], downcast = 'integer')
     print(numeric_train_df)
 
-    real = numeric_train_df.to_csv()
+    train_target = numeric_train_df.pop('Winner')
+    dataset = tf.data.Dataset.from_tensor_slices((numeric_train_df.values, train_target.values))
 
-    test = pd.read_csv(real)
+    train_dataset = dataset.shuffle(len(numeric_train_df)).batch(1)
 
-    print(test.head())
     
+    def get_compiled_model():
+        model = tf.keras.Sequential([
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(1)
+        ])
 
+        model.compile(optimizer='adam',
+                        loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                        metrics=['accuracy']
+                    )
+        return model
+
+    model = get_compiled_model()
+    model.fit(train_dataset, epochs=1)
 
 if __name__ == "__main__":
     main()
