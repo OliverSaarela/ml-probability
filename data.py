e0e657d5674b7283a0007688ba32f66221c0eec3
OliverSaarela <43965623+OliverSaarela@users.noreply.github.com>
2020-02-27 22:22:29 +0200

    Todennäköisyyksien näyttäminen ja eri arvojen testaus

--- data.py
+++ data.py
@@ -9,6 +9,10 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
 def main():
+
+    # Making data easier to read
+    np.set_printoptions(precision=3, suppress=True)
+
     train_data_path = 'data/2000-2020-supershort.csv'
 
     
@@ -29,7 +33,7 @@ def main():
     print(numeric_train_df)
 
     # Splitting dataframe to train and test data
-    train, test = train_test_split(numeric_train_df, test_size = 0.1)
+    train, test = train_test_split(numeric_train_df, test_size = 0.2)
 
 
     target = train.pop('Winner')
@@ -41,15 +45,15 @@ def main():
     for feat, targ in dataset.take(5):
         print ('Features: {}, Target: {}'.format(feat, targ))
 
-    train_dataset = dataset.shuffle(len(train)).batch(1)
-    test_dataset = dataset_2.shuffle(len(test)).batch(1)
+    train_dataset = dataset.shuffle(len(train)).batch(3)
+    test_dataset = dataset_2.shuffle(len(test)).batch(3)
 
     
     def get_compiled_model():
         model = tf.keras.Sequential([
             tf.keras.layers.Dense(128, activation='relu'),
-            tf.keras.layers.Dense(10, activation='relu'),
-            tf.keras.layers.Dense(1)
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(1, activation='sigmoid')
         ])
 
         model.compile(optimizer='adam',
@@ -64,6 +68,13 @@ def main():
     test_loss, test_accuracy = model.evaluate(test_dataset)
 
     print('\n\nTest Loss {}, Test Accuracy {}'.format(test_loss, test_accuracy))
+
+    predictions = model.predict(test_dataset)
+
+    for prediction, winner in zip(predictions[:10], list(test_dataset)[0][1][:10]):
+        print("Predicted P1 W: {:.2%}".format(prediction[0]),
+            " | Actual outcome: ",
+            ("P1" if bool(winner) else "P2"))
 
 if __name__ == "__main__":
     main()
