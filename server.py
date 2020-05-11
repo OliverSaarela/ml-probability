44ace62300c91d4d7f73796bdf4fff1b52c98668
Mikko Nurmi <mikko-nurmi@hotmail.fi>
2020-05-11 04:45:24 +0300

    added ability to server to deal with CORS

--- server.py
+++ server.py
@@ -1,26 +1,43 @@
 # coding=utf-8
 
 from http.server import BaseHTTPRequestHandler
+from flask_cors import CORS, cross_origin
 import json
 import data_server_interface
 import cgi
 
 
-class Server(BaseHTTPRequestHandler):
+
+class Server (BaseHTTPRequestHandler):
+
+
+    def end_headers (self):
+        BaseHTTPRequestHandler.end_headers(self)
 
         
     def _set_headers(self):
         self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', '*')
+        self.send_header("Access-Control-Allow-Headers", "*")
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
+        
     
     def do_HEAD(self):
         self._set_headers()
 
+
+    def do_OPTIONS(self):
+        self._set_headers()
+
     def do_POST(self):
 
-        ctype, pdict = cgi.parse_header(self.headers.get('Content-Type'))
         
+
+        ctype, pdict = cgi.parse_header(self.headers.get('Content-Type'))
+
+
         # refuse to receive non-json content
         if ctype != 'application/json':
             self.send_response(400)
@@ -34,7 +51,7 @@ class Server(BaseHTTPRequestHandler):
 	    #   "player2": "Federer R",
 	    #   "surface": "Hard"
         # }
-        lenght = int(self.headers.get('content-length'))
+        lenght = int(self.headers.get('content-length','Access-Control-Allow-Origin'))
         postdata = json.loads(self.rfile.read(lenght))
         
         #Make a predicton
@@ -45,7 +62,7 @@ class Server(BaseHTTPRequestHandler):
         posting = bytes(json.dumps(prediction), "UTF-8")
         
         #Send response
-
+        
         self._set_headers() 
         self.wfile.write(posting)
 
--- src/App.js
+++ src/App.js
@@ -1,52 +1,72 @@
-import React from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  return (
 
-    <body>
-    <div class="court">
-      <div class="court__grid">
-        <div class="court__cell court__alley--top-left">Doubles Alley</div>
-        <div class="court__cell court__alley--top-right">39 ft. &times; 4.5ft (175.5 sq. ft.)</div>
-        <div class="court__cell court__nml--left">"No Man's Land"</div>
-        <div class="court__cell court__ad--left">Ad Court<br />Left Service Box</div>
-        <div class="court__cell court__ad--right">21 ft. &times; 13.5 ft.<br/>(283.5 sq. ft.)</div>
-        <div class="court__cell court__dc--left">Deuce Court<br />Left Service Box </div>
-        <div class="court__cell court__dc--right">21 ft. &times; 13.5 ft.<br/>(283.5 sq. ft.)</div>
-        <div class="court__cell court__nml--right">18 ft. &times; 27 ft.<br/>(486 sq. ft.)</div>
-        <div class="court__cell court__alley--bottom-left">
-          <button>"prepare to be amazed"</button>
+
+  const [stringy, setValues] = useState( {
+  responseString: '',
+});
+
+  const dopost = (e) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json", "Access-Control-Allow-Headers");
+
+    var raw = JSON.stringify({
+      'player1': 'Nadal R',
+      'player2': 'Federer R',
+      'surface': 'Clay',
+    });
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+    };
+
+    fetch("http://localhost:8080", requestOptions)
+      .then(response => response.json())
+      .then(result => console.log(result))
+
+
+      .then(response => stringy.setValues({responseString: response}))
+
+      .catch(error => console.log('error', error));
+
+
+
+
+    };
+
+    return (
+
+      <body>
+      <div class="court">
+        <div class="court__grid">
+          <div class="court__cell court__alley--top-left">Doubles Alley</div>
+          <div class="court__cell court__alley--top-right">39 ft. &times; 4.5ft (175.5 sq. ft.)</div>
+          <div class="court__cell court__nml--left">"No Man's Land"</div>
+          <div class="court__cell court__ad--left">Ad Court<br />Left Service Box</div>
+          <div class="court__cell court__ad--right">21 ft. &times; 13.5 ft.<br/>(283.5 sq. ft.)</div>
+          <div class="court__cell court__dc--left">Deuce Court<br />Left Service Box </div>
+          <div class="court__cell court__dc--right">21 ft. &times; 13.5 ft.<br/>(283.5 sq. ft.)</div>
+          <div class="court__cell court__nml--right">answer here<br/>{stringy.responseString}</div>
+          <div class="court__cell court__alley--bottom-left">
+            <button onClick={dopost}>"prepare to be amazed"</button>
+
+
+
+          </div>
+          <div class="court__cell court__alley--bottom-right">39 ft. &times; 4.5ft (175.5 sq. ft.)</div>
         </div>
-        <div class="court__cell court__alley--bottom-right">39 ft. &times; 4.5ft (175.5 sq. ft.)</div>
       </div>
-    </div>
+      </body>
+    );
 
-      <label>Choose your fighter : </label>
-      <select id = "player1" onchange="change_player1(this.value)">
-        <option value = "Lad1">Lad1</option>
-        <option value = "Lad2">Lad2</option>
-        <option value = "Lad3">Lad3</option>
-      </select>
-      <br />
-      <label>Choose your opponent : </label>
-      <select id = "player2" onchange="change_player2(this.value)">
-        <option value = "Lad1">Lad1</option>
-        <option value = "Lad2">Lad2</option>
-        <option value = "Lad3">Lad3</option>
-      </select>
-      <br />
-      <label>Choose your arena : </label>
-      <select id = "surface" onchange="change_surface(this.value)">
-        <option value = "soft">soft</option>
-        <option value = "hard">hard</option>
-        <option value = "clay">clay</option>
-        <option value = "grass">grass</option>
-      </select>
-    </body>
-  );
+    };
 
-}
+
 
 export default App;
